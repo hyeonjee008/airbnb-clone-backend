f5ef131f43e9e0f024ea06f5788e2de0dd0e611f
hyeonjee008 <hyeonjee008@gmail.com>
2023-11-02 02:52:24 +0900

    ModelViewSet 사용(APIView 쓰는게 기본 But 커스텀 필요없는 경우 유용)

--- categories/urls.py
+++ categories/urls.py
@@ -1,7 +1,33 @@
 from django.urls import path
 from . import views
 
+
+# ModelViewSet 사용 시
 urlpatterns = [
-    path("", views.Categories.as_view()),
-    path("<int:pk>", views.CategoryDetail.as_view()),
+    path(
+        "",
+        views.CategoryViewSet.as_view(
+            {
+                "get": "list",
+                "post": "create",
+            }
+        ),
+    ),
+    path(
+        "<int:pk>",
+        views.CategoryViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+    ),
 ]
+
+
+# 기본 APIView 사용시 (ModelViewSet 사용 안할 시)
+# urlpatterns = [
+#     path("", views.Categories.as_view()),
+#     path("<int:pk>", views.CategoryDetail.as_view()),
+# ]
--- categories/views.py
+++ categories/views.py
@@ -3,54 +3,62 @@ from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from .models import Category
 from .serializers import CategorySerializer
 
 
-class Categories(APIView):
-    def get(self, request):
-        all_categories = Category.objects.all()
-        # Django에서 JSON으로 번역하고 싶으면 CategorySerializer에 category를 넘김
-        serializer = CategorySerializer(all_categories, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        # user로부터 데이터 가져오고 싶으면 data를 CategorySerializer에 넘김
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            new_category = serializer.save()
-            return Response(
-                CategorySerializer(new_category).data,
-            )
-        else:
-            return Response(serializer.errors)
+# ModelViewSet 사용 시 - 커스텀 필요 없을 경우 유용
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
 
 
-class CategoryDetail(APIView):
-    # DRF의 컨벤션
-    def get_object(self, pk):
-        try:
-            category = Category.objects.get(pk=pk)
-        except Category.DoesNotExist:
-            raise NotFound
-        return category
+# 기본 APIView 사용시 (ModelViewSet 사용 안할 시) - 커스텀 필요한 경우 이게 더 나음
+# class Categories(APIView):
+#     def get(self, request):
+#         all_categories = Category.objects.all()
+#         # Django에서 JSON으로 번역하고 싶으면 CategorySerializer에 category를 넘김
+#         serializer = CategorySerializer(all_categories, many=True)
+#         return Response(serializer.data)
 
-    def get(self, request, pk):
-        serializer = CategorySerializer(self.get_object(pk))
-        return Response(serializer.data)
+#     def post(self, request):
+#         # user로부터 데이터 가져오고 싶으면 data를 CategorySerializer에 넘김
+#         serializer = CategorySerializer(data=request.data)
+#         if serializer.is_valid():
+#             new_category = serializer.save()
+#             return Response(
+#                 CategorySerializer(new_category).data,
+#             )
+#         else:
+#             return Response(serializer.errors)
 
-    def put(self, request, pk):
-        serializer = CategorySerializer(
-            self.get_object(pk),
-            data=request.data,
-            partial=True,  # 필수인자가 없어도 valid()가 가능하게 함
-        )
-        if serializer.is_valid():
-            updated_category = serializer.save()
-            return Response(CategorySerializer(updated_category).data)
-        else:
-            return Response(serializer.errors)
 
-    def delete(self, request, pk):
-        self.get_object(pk).delete()
-        return Response(status=HTTP_204_NO_CONTENT)
+# class CategoryDetail(APIView):
+#     # DRF의 컨벤션
+#     def get_object(self, pk):
+#         try:
+#             category = Category.objects.get(pk=pk)
+#         except Category.DoesNotExist:
+#             raise NotFound
+#         return category
+
+#     def get(self, request, pk):
+#         serializer = CategorySerializer(self.get_object(pk))
+#         return Response(serializer.data)
+
+#     def put(self, request, pk):
+#         serializer = CategorySerializer(
+#             self.get_object(pk),
+#             data=request.data,
+#             partial=True,  # 필수인자가 없어도 valid()가 가능하게 함
+#         )
+#         if serializer.is_valid():
+#             updated_category = serializer.save()
+#             return Response(CategorySerializer(updated_category).data)
+#         else:
+#             return Response(serializer.errors)
+
+#     def delete(self, request, pk):
+#         self.get_object(pk).delete()
+#         return Response(status=HTTP_204_NO_CONTENT)
